958ef8898a214d7087e7b721c04ce2aaf519bbf7
Roan Mertens <roanmertens91@gmail.com>
2018-09-25 22:48:21 +0100

    ask user for two strings to compare

--- compare-char.rb
+++ compare-char.rb
@@ -3,10 +3,16 @@
 # E.g, for "harpoon" and "countryside", the answer is "nro".
 
 def comp_char(str_one, str_two)
+  print "In #{str_one} and #{str_two} are the letters: "
   letters = str_one.split('').select do |letter|
     str_two.split('').include?(letter)
   end
   letters.uniq.join('')
 end
 
-puts comp_char('harpoon', 'countryside')
+print 'word one:'
+char_one = gets.chomp
+print 'word two:'
+char_two = gets.chomp
+
+puts comp_char(char_one, char_two)
