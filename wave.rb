8e84bfeb4f424fd5e39199208b8dcbeeed6cc85c
Roan Mertens <roanmertens91@gmail.com>
2018-11-26 19:56:53 +0000

    made it so empty spaces are skipped

--- wave.rb
+++ wave.rb
@@ -7,9 +7,9 @@ def wave(str)
   str.length.times { flat << str }
 
   flat.each_with_index do |string, i|
-    wave << string.gsub(string[i], string[i].upcase)
+    wave << string.gsub(string[i], string[i].upcase) unless string[i] == ' '
   end
   wave
 end
 
-p wave('string')
+p wave('str ing')
