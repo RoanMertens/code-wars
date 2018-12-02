170c77e01b8e2545798a4e4ea149c5e033ab0392
Roan Mertens <roanmertens91@gmail.com>
2018-12-02 15:38:40 +0000

    finished the like-system method

--- like-system.rb
+++ like-system.rb
@@ -1,7 +1,25 @@
-
 # a method that returns a string with the names that are given.
 def likes(people)
-  return 'no one likes this' if people == []
+  if people.length.zero?
+    'no one likes this'
+  elsif people.length == 1
+    people[0] + ' likes this'
+  else
+    long_likes(people)
+  end
+end
+
+def long_likes(people)
+  a = ''
+  people.each_with_index do |name, index|
+    a += name + ', ' if index < people.length - 2
+  end
+  a + people[-2] + ' and ' + people[-1] + ' like this'
 end
 
 puts likes([])
+puts likes(['bobby'])
+puts likes(%w[bobby bert])
+puts likes(%w[bobby bert jerry frank])
+
+# "Jacob and Alex like this"
